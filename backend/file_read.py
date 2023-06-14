c2602f1bc4ad89f9975b98638241aa87da8897f5
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-06-14 12:26:24 -0500

    Update file_read.py

--- backend/file_read.py
+++ backend/file_read.py
@@ -1,16 +1,5 @@
-wordList = []
-
 with open('dependent.txt', 'r') as file:
   for line in file:
     for word in line.split():
-      if word == 'by':
-        continue
-      else:
-        wordList.append(word)
+      print(word, end="\t")
 file.close()
-
-for i in range(len(wordList)):
-  if i % 5 == 0:
-    print(f'\n{wordList[i]}', end='\t\t\t\t')
-  else:
-    print(f'{wordList[i]}', end='\t\t\t\t')
