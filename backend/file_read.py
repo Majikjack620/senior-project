2d52d168ac91eddf23f03bd138e7588e4e887f99
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-06-14 15:05:48 -0500

    Update file_read.py

--- backend/file_read.py
+++ backend/file_read.py
@@ -1,10 +1,18 @@
 with open('dependent.txt', 'r') as file:
+  first_line = file.readline().strip("\n")
+  
   for line in file:
     listings = list(line.split())
     length = len(listings)
     
     for i in range(length):
-      if i == length-1:
-        print(listings[i], end=" | \n")
+      if listings[i] == "by":
+        continue
       else:
-        print(listings[i], end=" | ")
+        if i == length-1:
+          print(listings[i], end=" | \n")
+        else:
+          print(listings[i], end=" | ")
+          
+    if line == first_line:
+      print("| ----------- | ----------- | ----------- | ----------- | ----------- | ----------- |")
