dda5e052ba000998d42c68e52d682b1298b3d36f
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-06-14 12:31:14 -0500

    Update file_read.py

--- backend/file_read.py
+++ backend/file_read.py
@@ -1,5 +1,9 @@
+divider = "              |              "
+
 with open('dependent.txt', 'r') as file:
   for line in file:
     for word in line.split():
-      print(word, end="\t")
+      print(word, end=divider)
+
+    print("\n")
 file.close()
