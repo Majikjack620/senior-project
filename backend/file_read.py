391f7ad80a765f6d95528326daf2cb3926cbceb4
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-06-14 12:40:18 -0500

    Delete file_read.py

--- backend/file_read.py
+++
@@ -1,9 +0,0 @@
-divider = "              |              "
-
-with open('dependent.txt', 'r') as file:
-  for line in file:
-    for word in line.split():
-      print(word, end=divider)
-
-    print("\n")
-file.close()
