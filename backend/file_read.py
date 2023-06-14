31d2f7097378dc15a5ca946cdcad1a627ede752c
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-06-14 15:19:06 -0500

    Update file_read.py

--- backend/file_read.py
+++ backend/file_read.py
@@ -4,12 +4,9 @@ with open('dependent.txt', 'r') as file:
     length = len(sep)
     
     for i in range(length):
-      if sep[i] == "by":
-        continue
+      if i == length-1:
+        print(sep[i], end=" | \n")
       else:
-        if i == length-1:
-          print(sep[i], end=" | \n")
-        else:
-          print(sep[i], end=" | ")
+        print(sep[i], end=" | ")
 
-    print("| - | - | - | - | - | - | - |")
+    print("| - | - | - | - | - | - |")
