361c9c3a32e57429e10baba9cff9a2cb753ecb29
Majikjack620 <Majikjack620@users.noreply.github.com>
2023-08-09 22:42:01 +0000

    :art: Formatting Python code with psf/black

--- backend/check_pass_fail.py
+++ backend/check_pass_fail.py
@@ -1,5 +1,5 @@
-with open('dependent.txt', 'r') as checker:
-  content = checker.read()
-  
-  if 'Minor' in content:
-    exit("Cannot merge: Minor dependency update(s) available")
+with open("dependent.txt", "r") as checker:
+    content = checker.read()
+
+    if "Minor" in content:
+        exit("Cannot merge: Minor dependency update(s) available")
