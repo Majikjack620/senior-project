3507554f0fab7dc55b79ffd857a27cb648a30be0
Majikjack620 <Majikjack620@users.noreply.github.com>
2023-08-10 22:54:37 +0000

    refactor: :chart_with_upwards_trend::art: SQL Files reformatted to generalize convention

--- test.sql
+++ test.sql
@@ -4,10 +4,9 @@ SELECT game,
 FROM halo_comp
 WHERE game == "CTF";
 
-CREATE OR REPLACE TABLE hello_world
 
-CREATE OR REPLACE 
-	FUNCTION public.f_epoch_to_datetime(epoch int8) RETURNS timestamp LANGUAGE plpythonu IMMUTABLE AS $$
+CREATE OR REPLACE TABLE hello_world
+CREATE OR REPLACE FUNCTION public.f_epoch_to_datetime(epoch int8) RETURNS timestamp LANGUAGE plpythonu IMMUTABLE AS $$
 
 	from datetime import datetime
 	return datetime.fromtimestamp(epoch // 1000)
