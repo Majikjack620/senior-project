07322eceb02aa4ed3acacc5d1876b43d92975476
Majikjack620 <Majikjack620@users.noreply.github.com>
2023-08-10 00:59:55 +0000

    :chart_with_upwards_trend::art: SQL Files reformatted to common format

--- test.sql
+++ test.sql
@@ -1,14 +1,11 @@
 SELECT game,
        MODE,
-       MONEY FROM halo_comp WHERE game == "CTF";
+       MONEY
+FROM halo_comp
+WHERE game == "CTF";
 
 
-CREATE OR REPLACE 
-	FUNCTION public.f_epoch_to_datetime(epoch int8) 
-	RETURNS timestamp 
-	LANGUAGE plpythonu 
-	IMMUTABLE 
-	AS $$
+CREATE OR REPLACE FUNCTION public.f_epoch_to_datetime(epoch int8) RETURNS timestamp LANGUAGE plpythonu IMMUTABLE AS $$
 
 	from datetime import datetime
 	return datetime.fromtimestamp(epoch // 1000)
