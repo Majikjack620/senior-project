a110d8858023b3d3a1fc521575cd1aebcfee8404
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-08-09 20:23:59 -0500

    Update check_pass_fail.py

--- backend/check_pass_fail.py
+++ backend/check_pass_fail.py
@@ -1,5 +1,6 @@
-with open("dependent.txt", "r") as checker:
+with open("dependent.txt", 'r') as checker:
     content = checker.read()
 
-    if "Minor" in content:
-        exit("Cannot merge: Minor dependency update(s) available")
+    if 'Minor' in content:
+        exit('Cannot merge: Minor dependency update(s) available'
+        )
