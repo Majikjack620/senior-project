89ceac351e7e450553700e7b0d5d6a2b79a4c370
Majikjack620 <Majikjack620@users.noreply.github.com>
2023-08-10 01:31:31 +0000

    :snake::art: Python Files Reformatted to common format

--- backend/file_read.py
+++ backend/file_read.py
@@ -60,18 +60,20 @@ with open("dependent.txt", "r") as depend:
 depend.close()
 
 # Open file again for clean reading
-with open("dependent.txt", 'r') as depend:
+with open("dependent.txt", "r") as depend:
     content = depend.read()
 
     # Print a line
     print("##")
 
     # Check for major updates
-    if 'Major' in content:
+    if "Major" in content:
         print(
             "#### :warning: MAJOR DEPENDENCY UPDATE(S): Make sure to check the dependency docs before updating :warning:"
         )
 
     # Check for minor updates
-    if 'Minor' in content:
-        print("#### :x: MINOR DEPENDENCY UPDATE(S): Update these dependencies, otherwise the check will keep failing :x:")
+    if "Minor" in content:
+        print(
+            "#### :x: MINOR DEPENDENCY UPDATE(S): Update these dependencies, otherwise the check will keep failing :x:"
+        )
