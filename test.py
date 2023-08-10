4bfa6d50acc9bcd702dccd6fa6d7f08c6ef73b95
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-08-10 17:35:25 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
-testing = ["apple", "banana", "orange", "hot dog", "lettuce", "ham", "cheddar"]
+testing = ["apple", "banana", "orange", "hot dog", "lettuce", "ham", 'cheddar']
 for x in testing:
     print(f"{x} is a food")
 
 five = 5
-six = 6
+six = 9
 eleven = five + six
 
 print(f"{five} + {six} = {eleven}")
 
 not_used = "hello"
 
-print(
-    "making a super super super super super long string to get past 80 characters while testing the Black Python Formatter."
-)
+print("making a super super super super super long string to get past 80 characters while testing the Black Python Formatter.")
