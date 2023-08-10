9038323466140cb7c6f55d5b96444062d11c47a8
Majikjack620 <Majikjack620@users.noreply.github.com>
2023-08-10 15:21:49 +0000

    refactor: :snake::art: Python Files reformatted to generalize convention

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 testing = ["apple", "banana", "orange", "hot dog", "lettuce", "ham", "cheddar"]
 for x in testing:
-  print(f'{x} is a food')
+    print(f"{x} is a food")
 
 five = 5
 six = 6
 eleven = five + six
 
-print(f'{five} + {six} = {eleven}'
-     )
+print(f"{five} + {six} = {eleven}")
 
 not_used = "hello"
 
-print("making a super super super super super long string to get past 80 characters while testing the Black Python Formatter.")
+print(
+    "making a super super super super super long string to get past 80 characters while testing the Black Python Formatter."
+)
