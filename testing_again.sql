294509fa7dbb57f4825f9d50d46dfdc69469c858
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-08-11 12:28:19 -0500

    test: update testing_again.sql

--- testing_again.sql
+++ testing_again.sql
@@ -15,3 +15,14 @@ CREATE OR REPLACE FUNCTION hello(world int8, goodbye string RETURNS int8 LANGUAG
 			import datetime
 			return datetime
 $$ ;
+
+SELECT *,
+       count(games) FROM halo_comp GROUP BY comp_game
+
+	
+CREATE OR REPLACE FUNCTION public.f_epoch_to_datetime(epoch int8) RETURNS timestamp LANGUAGE plpythonu IMMUTABLE AS $$
+
+	from datetime import datetime
+	return datetime.fromtimestamp(epoch // 1000)
+
+$$;
