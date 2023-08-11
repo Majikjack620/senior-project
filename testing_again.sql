75a7580437a63397d96fd33ca0cdfe0cc06c793b
Joshua Jackson (He/Him) <122300334+Majikjack620@users.noreply.github.com>
2023-08-11 08:41:23 -0500

    Create testing_again.sql

---
+++ testing_again.sql
@@ -0,0 +1,20 @@
+SELECT game,
+       MODE,
+       MONEY FROM halo_comp WHERE game == "CTF";
+
+
+CREATE OR REPLACE TABLE hello_world
+  
+CREATE OR REPLACE FUNCTION public.f_epoch_to_datetime(epoch int8) RETURNS timestamp LANGUAGE plpythonu IMMUTABLE AS $$
+
+	from datetime import datetime
+	return datetime.fromtimestamp(epoch // 1000)
+
+$$;
+
+CREATE OR REPLACE FUNCTION public.f_epoch_toe(epoch int8) RETURNS timestamp LANGUAGE plpythonu IMMUTABLE AS $$
+
+	from datetime import datetime
+	return datetime.fromtimestamp(epoch // 1000)
+
+$$;
