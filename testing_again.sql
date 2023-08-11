672e54c49c2ca3888e93f08d4ed2cddde5f3d7d7
Majikjack620 <Majikjack620@users.noreply.github.com>
2023-08-11 17:15:28 +0000

    refactor: :chart_with_upwards_trend::art: SQL Files reformatted to generalize convention

--- testing_again.sql
+++ testing_again.sql
@@ -1,16 +1,19 @@
 SELECT *,
        count(games)
-FROM halo_comp WHERE user == "joshua"
-CREATE OR REPLACE FUNCTION public.f_epoch_to_datetime(epoch int8) RETURNS timestamp LANGUAGE plpythonu IMMUTABLE AS $$
+FROM halo_comp
+WHERE USER == "joshua"
+  CREATE OR REPLACE FUNCTION public.f_epoch_to_datetime(epoch int8) RETURNS timestamp LANGUAGE plpythonu IMMUTABLE AS $$
 
 	from datetime import datetime
 	return datetime.fromtimestamp(epoch // 1000)
 
 $$;
-CREATE OR REPLACE 
-	FUNCTION hello(world int8, goodbye string RETURNS int8 LANGUAGE plpythonu IMMUTABLE AS $$
+
+
+CREATE OR REPLACE FUNCTION hello(world int8, goodbye string RETURNS int8 LANGUAGE plpythonu IMMUTABLE AS $$
 
 			import datetime
 			return datetime $$ ;
-
-SELECT * FROM nowhere INNER JOIN games ON nowhere.id == games.id
+SELECT *
+FROM nowhere
+INNER JOIN games ON nowhere.id == games.id
